b76bb0fa9c883d7624e9aeed8cae3c43a7f36aa9
Jonathan Souza <souzajonathan570@gmail.com>
2022-02-21 22:34:56 -0300

    feat: add theme to browser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import GlobalStyle from './GlobalStyle';
 import LocationDisplay from './helpers/LocationDisplay';
 import Routes from './routes/Routes';
 
-const App = () => (
-  <div>
-    <Routes />
-    <GlobalStyle />
-    <ToastContainer
-      position="top-right"
-      autoClose={ 5000 }
-      hideProgressBar={ false }
-      newestOnTop={ false }
-      closeOnClick
-      rtl={ false }
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-    />
-    <LocationDisplay />
-  </div>
-);
+const App = () => {
+  useEffect(() => {
+    document.title = 'Crypto Index';
+  });
+  return (
+    <div>
+      <Routes />
+      <GlobalStyle />
+      <ToastContainer
+        position="top-right"
+        autoClose={ 5000 }
+        hideProgressBar={ false }
+        newestOnTop={ false }
+        closeOnClick
+        rtl={ false }
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <LocationDisplay />
+    </div>
+  );
+};
 
 export default App;
